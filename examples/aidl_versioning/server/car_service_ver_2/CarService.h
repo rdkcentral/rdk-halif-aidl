aaeec86e34c435b85631efaedc58df13cd44f127
Aniket Patil <78640016+aniketp1809@users.noreply.github.com>
2025-09-15 06:38:28 +0000

    Feature: AIDL Versioning for RDK-E HAL #75
    1. Updated CMake configurations of example code to support
    the updated directory structure
    2. Added a script to compile interfaces stubs and proxies
    3. Updated Notice and added header for all files to resolve licensing issues.
    
    Reviewed by: Gerald Weatherup, Varghese Mathai, Rahul Raveendran
    JIRA URL: https://github.com/rdkcentral/rdk-halif-aidl/issues/75

--- examples/aidl_versioning/server/car_service_ver_2/CarService.h
+++ examples/aidl_versioning/server/car_service_ver_2/CarService.h
@@ -1,10 +1,28 @@
+/**
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #ifndef CAR_IMPL_H
 #define CAR_IMPL_H
 
-#include <com/demo/hal/car/BnCar.h>
-#include <com/demo/hal/car/CarSpecs.h>
-#include <com/demo/hal/car/CarStatus.h>
-#include <com/demo/hal/car/ICarStatusListener.h>
+#include <2/com/demo/hal/car/BnCar.h>
+#include <2/com/demo/hal/car/CarSpecs.h>
+#include <2/com/demo/hal/car/CarStatus.h>
+#include <2/com/demo/hal/car/ICarStatusListener.h>
 #include <com/demo/hal/vehicle/VehicleStatus.h>
 #include <com/demo/hal/vehicle/VehicleSpecs.h>
 #include <com/demo/hal/common/FuelStatus.h>
