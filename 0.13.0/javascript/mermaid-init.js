f141d7131f1a7e51e68714377059bafdb0fb3494
Ulrond <131959864+Ulrond@users.noreply.github.com>
2026-01-07 15:25:24 +0000

    Removed 0.13.0 with mike 2.1.3

--- 0.13.0/javascript/mermaid-init.js
+++
@@ -1,7 +0,0 @@
-document$.subscribe(function() {
-    mermaid.initialize({
-        startOnLoad: true,
-        theme: "default",  // Or "dark" if using dark mode
-        securityLevel: "loose"
-    });
-});
